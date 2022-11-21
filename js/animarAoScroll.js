f0806d458da361c6b9ba8b95dd3238cffbbf4794
MatheusBaganha <matheus.baganha1@gmail.com>
2022-11-21 18:09:21 -0300

    Animar ao Scroll com Intersection Observer

--- js/animarAoScroll.js
+++ js/animarAoScroll.js
@@ -1,5 +1,18 @@
 export default function animarAoScroll() {
-  const observer = new IntersectionObserver(entries => {
-    console.log(entries)
+  const textsToBeAnimated = document.querySelectorAll('[data-animar] p, [data-animar] .containerPergunta');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle('show', entry.isIntersecting)
+      if  (entry.isIntersecting) {
+        observer.unobserve(entry.target)
+      }
+    })
+  }, {
+    threshold: 1,
+  })
+
+  textsToBeAnimated.forEach((text) => {
+    observer.observe(text);
   })
 }
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 import menuMobile from './js/menuMobile.js';
+import animarAoScroll from './js/animarAoScroll.js';
 
 menuMobile();
+animarAoScroll();

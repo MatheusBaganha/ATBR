963ba13c8acaaa08018ccec83a291e81b972532d
MatheusBaganha <matheus.baganha1@gmail.com>
2022-11-26 15:45:31 -0300

    feature formContato

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ import animarAoScroll from './js/animarAoScroll.js';
 import menuMobile from './js/menuMobile.js';
 import toggleDuvidas from './js/toggleDuvidas.js';
 import scrollSuave from './js/scrollSuave.js';
+import formContato from './js/formContato.js';
 
 animarAoScroll();
 menuMobile();
 scrollSuave();
 toggleDuvidas();
+formContato();
